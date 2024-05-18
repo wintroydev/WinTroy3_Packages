f51c828706457c669c215d1c7088a4430a9b9650
Hobbylos <567Aslan567@gmail.com>
2024-05-18 21:17:12 +0200

    web removed

--- Web/App/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('button').addEventListener('click', function() {
-    const botToken = document.getElementById('botToken').value;
-    window.chrome.webview.postMessage(`botToken:${botToken}`);
-});
